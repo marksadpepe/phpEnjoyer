98f9e863504c7bdd554355ce76cae837aa9ac760
Mark Parker <marksadpepe@gmail.com>
2024-05-09 02:24:56 +0300

    added route for registration

--- routes.php
+++ routes.php
@@ -7,7 +7,7 @@ $router = new Router();
 $router->set_namespace("Blog\\Controllers");
 $router->set_middleware_namespace("Blog\\Middlewares");
 
-//$router->post("/sign-up", "AuthController::sign_up");
+$router->post("/sign-up", null, "AuthController::sign_up");
 $router->post("/sign-in", null, "AuthController::sign_in");
 $router->post("/sign-out", null, "AuthController::sign_out");
 
