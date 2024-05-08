240fc24d329b91118d44f118eb31b400086c11f9
Mark Parker <marksadpepe@gmail.com>
2024-05-08 01:21:29 +0000

    added config file as an example

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$DB_HOST = database_host;
+$DB_USERNAME = database_username;
+$DB_PASSWORD = database_password;
+$DB_NAME = database_name;
+$DB_PORT = database_port_number;
+$TOKEN_TTL = token_time_to_live_number;
+?>
