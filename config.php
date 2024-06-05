c4f0e5722b5bcd7e80b56398aea8140961ed1fda
Mark Parker <marksadpepe@gmail.com>
2024-06-05 21:56:23 +0000

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$DB_HOST = database_host;
-$DB_USERNAME = database_username;
-$DB_PASSWORD = database_password;
-$DB_NAME = database_name;
-$DB_PORT = database_port_number;
-$TOKEN_TTL = token_time_to_live_number;
-?>
